4af39de56363ba4b8f438d9049c3b2888e816dd7
puresmash <puresmash@gmail.com>
2017-11-06 02:55:14 +0800

    Testing publish to npm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SlidePlayer from './examples/SlidePlayer';
 
-ReactDOM.render(<SlidePlayer />, document.getElementById('root'));
+import ImageManager from './ImageManager';
+
+export {
+  ImageManager
+}
