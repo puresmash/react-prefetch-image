44de88a7792743ef1aa2727a941ee05a6ae7609a
puresmash <puresmash@gmail.com>
2017-11-06 02:35:04 +0800

    Init develop branch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@ import ReactDOM from 'react-dom';
 import SlidePlayer from './examples/SlidePlayer';
 
 ReactDOM.render(<SlidePlayer />, document.getElementById('root'));
-
-
-// import ImageManager from './ImageManager';
-//
-// export {
-//   ImageManager
-// }
